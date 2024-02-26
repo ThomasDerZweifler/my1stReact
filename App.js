a4b0da35780afc25b76f118d5c4af79a265e1719
ThoFu <thomasfunke71@googlemail.com>
2024-02-26 09:04:13 +0100

    add readme and untracked files

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import { OwnComponent } from './OwnComponent.tsx'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image
+        source={{
+          uri: 'https://reactnative.dev/docs/assets/p_cat1.png',
+        }}
+        style={{width: 200, height: 200}}
+      />
+
+      <OwnComponent/>
+
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
